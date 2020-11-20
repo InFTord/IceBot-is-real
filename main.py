bf8b9af3433c4b5b3a69403021c0fa5cdaf4b24a
InFTord <60395110+InFTord@users.noreply.github.com>
2020-11-20 13:50:55 +0500

    Добавлен начальный юзеринфо, обновлен префикс и добавлены примеры использования

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 
 import discord
 from discord.ext import commands
+from typing import Optional
 from discord.ext.commands.errors import CommandNotFound
+from discord.member import Member
 from config import bot_token, prefix
 
 
@@ -13,7 +15,7 @@ client = commands.Bot(command_prefix=prefix, case_insensitive=True, intents = di
 @client.event
 async def on_ready():
     print(f'{client.user.name} в сети')
-    await client.change_presence(activity=discord.Game(name='>help'))
+    await client.change_presence(activity=discord.Game(name='i!help'))
 
 
 @client.event
@@ -30,19 +32,25 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('Понг!')
 
-@client.command(name='очистка', aliases=['clear', 'c', 'очистить'])
+@client.command(name='очистка', aliases=['clear', 'c', 'очистить'], usage='[Количество]')
 @commands.has_guild_permissions(manage_messages = True)
 async def clear(ctx, amount=2):
     await ctx.channel.purge(limit = amount)
     await ctx.send('✅')
 
 
-@client.command(name='кик', aliases=['kick', 'выгнать', 'k'])
+@client.command(name='кик', aliases=['kick', 'выгнать', 'k'], usage='[участник] {причина}')
 @commands.has_guild_permissions(kick_members = True)
 async def kick(ctx, member: discord.Member, *, reason="Причины не дано"):
     await member.kick(reason=reason)
     await ctx.message.add_reaction('✅')
     await member.send(f'Вы были кикнуты с {ctx.guild} по причине {reason}')
+
+@client.command(name='юзеринфо', aliases=['user', 'userinfo', 'профиль', 'u', 'profile'], usage='{пользователь}')
+async def user(ctx, member: Optional[Member]):
+    member = member or ctx.author
+    embed = discord.Embed(description=f'Имя юзера: {member.display_name}\nИД: {member.id}\n', color=member.color)
+    await ctx.send(embed=embed)
 
 # Логин бота
 
